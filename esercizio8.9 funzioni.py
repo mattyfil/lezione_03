3c53c8a68016d25e61cbb9be1ca46da0c5ff3cb3
mattyfil <luigi.cona@hotmail.it>
2025-03-11 12:01:17 +0100

    Add files via upload

---
+++ esercizio8.9 funzioni.py
@@ -0,0 +1,23 @@
+frasi:list=[
+    "ho molto sonno",
+    "ho molta fame",
+    "devo studiare di +"
+]
+
+
+
+def show_message(frasi:str)->str:
+    for i in frasi:
+        print(i)
+
+show_message(frasi)
+
+    
+    
+    
+
+
+
+
+
+    
